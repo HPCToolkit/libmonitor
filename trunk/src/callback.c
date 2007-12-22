975be4f29285e165a23625ca6d81cfbcfa2a040b
krentel <krentel@55dce0e8-169e-9cfc-bb57-5d3f7de1ffaf>
2007-12-22 21:01:48 +0000

    Preliminary support for MPI.  Provides callbacks for C versions of MPI_Init() and MPI_Finalize() in the dynamic LD_PRELOAD case. Doesn't support Fortran versions or the static case yet.

--- trunk/src/callback.c
+++ trunk/src/callback.c
@@ -67,3 +67,21 @@ monitor_dlopen(const char *library)
 {
     MONITOR_DEBUG("(default callback) library = %s\n", library);
 }
+
+void __attribute__ ((weak))
+monitor_init_mpi(int *argc, char ***argv)
+{
+    int i;
+
+    MONITOR_DEBUG("(default callback) size = %d, rank = %d, argc = %d\n",
+		  monitor_mpi_comm_size(), monitor_mpi_comm_rank(), *argc);
+    for (i = 0; i < *argc; i++) {
+	MONITOR_DEBUG("argv[%d] = %s\n", i, (*argv)[i]);
+    }
+}
+
+void __attribute__ ((weak))
+monitor_fini_mpi(void)
+{
+    MONITOR_DEBUG1("(default callback)\n");
+}
