e8de426a200eaff1b10402eff0b6a00d4f21d051
krentel <krentel@55dce0e8-169e-9cfc-bb57-5d3f7de1ffaf>
2008-02-27 21:35:45 +0000

    Add monitor_real_exit() as a tunnel to the real _exit(), in case the client needs to exit the process.

--- trunk/src/monitor.h
+++ trunk/src/monitor.h
@@ -68,6 +68,7 @@ extern void monitor_fini_mpi(void);
  */
 extern void *monitor_real_dlopen(const char *path, int flags);
 extern int monitor_real_dlclose(void *handle);
+extern void monitor_real_exit(int status);
 extern int monitor_real_sigprocmask(int how, const sigset_t *set,
 				    sigset_t *oldset);
 extern int monitor_real_pthread_sigmask(int how, const sigset_t *set,
